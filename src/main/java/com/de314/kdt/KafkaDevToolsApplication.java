ff9b0c4b355ea3d0e13e02c6bf0cfbd0387baed9
David Esposito <david.esposito@bettercloud.com>
2018-10-19 08:09:13 -0400

    Initial commit
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

---
+++ src/main/java/com/de314/kdt/KafkaDevToolsApplication.java
@@ -0,0 +1,12 @@
+package com.de314.kdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDevToolsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/de314/kdt/KafkaDevToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.de314.kdt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaDevToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
